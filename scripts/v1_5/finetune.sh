c10cfc843d79a191ce099afb3c980058b0e0bef6
xsl <xsl.cmd@gmail.com>
2024-01-17 03:08:25 +0800

    fix

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+PYTHONPATH='.' \
+srun -p s1_mm_research --ntasks-per-node=1 --gres=gpu:8 --cpus-per-task=10 --nodes=1 \
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+    --model_name_or_path checkpoints/vicuna-13b-v1.5 \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
-    --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-13b-pretrain/mm_projector.bin \
+    --data_path datasets/LLaVA-Instruct-150K/llava_v1_5_mix665k.json \
+    --image_folder ./datasets \
+    --vision_tower checkpoints/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter ./output/llava-v1.5-13b-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -15,7 +16,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b \
+    --output_dir ./output/llava-v1.5-13b \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -33,5 +34,4 @@ deepspeed llava/train/train_mem.py \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
-    --lazy_preprocess True \
-    --report_to wandb
+    --lazy_preprocess True

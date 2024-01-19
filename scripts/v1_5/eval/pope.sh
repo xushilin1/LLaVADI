9a62bb2666698194310347d916e0fbcd2b304dcb
xsl <xsl.cmd@gmail.com>
2024-01-19 16:04:42 +0800

    fix

--- scripts/v1_5/eval/pope.sh
+++ scripts/v1_5/eval/pope.sh
@@ -3,7 +3,7 @@
 python -m llava.eval.model_vqa_loader \
     --model-path liuhaotian/llava-v1.5-13b \
     --question-file ./playground/data/eval/pope/llava_pope_test.jsonl \
-    --image-folder ./playground/data/eval/pope/val2014 \
+    --image-folder ./datasets/coco/val2014 \
     --answers-file ./playground/data/eval/pope/answers/llava-v1.5-13b.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
--- scripts/v1_5/eval/textvqa.sh
+++ scripts/v1_5/eval/textvqa.sh
@@ -4,10 +4,10 @@ python -m llava.eval.model_vqa_loader \
     --model-path checkpoints/llava-v1.5-13b \
     --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
     --image-folder ./datasets/textvqa/train_images \
-    --answers-file ./datasets/eval/textvqa/answers/llava-v1.5-13b.jsonl \
+    --answers-file ./playground/data/eval/textvqa/answers/llava-v1.5-13b.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 python -m llava.eval.eval_textvqa \
     --annotation-file ./playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
-    --result-file ./datasets/eval/textvqa/answers/llava-v1.5-13b.jsonl
+    --result-file ./playground/data/eval/textvqa/answers/llava-v1.5-13b.jsonl

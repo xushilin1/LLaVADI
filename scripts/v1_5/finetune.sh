2c153780962081933d703fdd75fbeaec5bfa8b6c
xsl <xsl.cmd@gmail.com>
2024-02-02 14:51:16 +0800

    fix

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# SBATCH --job-name=test-nodes        # name
-# SBATCH --nodes=1                    # nodes
-# SBATCH --ntasks-per-node=1          # crucial - only 1 task per dist per node!
-# SBATCH --cpus-per-task=10           # number of cores per tasks
-# SBATCH --gres=gpu:8                 # number of gpus
-# SBATCH --time 20:00:00              # maximum execution time (HH:MM:SS)
-# SBATCH --output=%x-%j.out           # output file name
-# SBATCH --partition=s1_mm_research
-# SBATCH --quotatype=auto
+#SBATCH --job-name=deepspeed        # name
+#SBATCH --nodes=2                    # nodes
+#SBATCH --ntasks-per-node=1          # crucial - only 1 task per dist per node!
+#SBATCH --cpus-per-task=10           # number of cores per tasks
+#SBATCH --gres=gpu:8                 # number of gpus
+#SBATCH --time 20:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --output=%x-%j.out           # output file name
+#SBATCH --partition=s1_mm_research
+#SBATCH --quotatype=auto
 
 export GPUS_PER_NODE=8
 export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
@@ -17,14 +17,15 @@ PYTHONPATH='.' \
 srun --jobid $SLURM_JOBID bash -c 'python -m torch.distributed.run \
  --nproc_per_node $GPUS_PER_NODE --nnodes $SLURM_NNODES --node_rank $SLURM_PROCID \
  --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
-llava/train/train_mem.py \
+projects/distill/distill_train.py \
+    --align_hidden_embeds true \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path checkpoints/MobileLLaMA-2.7B-Chat \
     --version v1 \
     --data_path datasets/LLaVA-Instruct-150K/llava_v1_5_mix665k.json \
     --image_folder ./datasets \
     --vision_tower checkpoints/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ./output/llava-v1.5-13b-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter output/pretrain/MobileLLaMA-2.7B-Chat/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -32,9 +33,9 @@ llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./output/llava-v1.5-13b \
+    --output_dir ./output/distill/finetune/llava_MobileLLaMA-2.7B-Chat_exp11 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \

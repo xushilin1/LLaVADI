9157e93120ecc947991c6895963bccd7f7cd953a
Haobo Yuan <yuanhaobo@whu.edu.cn>
2024-02-25 19:57:51 +0800

    bugfix

--- projects/ext/mobilevlm/model/mobilellama.py
+++ projects/ext/mobilevlm/model/mobilellama.py
@@ -49,8 +49,9 @@ class MobileLlamaForCausalLM(LlamaForCausalLM, MobileVLMMetaForCausalLM):
         output_hidden_states = (output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states)
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        input_ids, attention_mask, past_key_values, inputs_embeds, labels = \
-            self.prepare_inputs_labels_for_multimodal(input_ids, attention_mask, past_key_values, labels, images)
+        if inputs_embeds is None:
+            input_ids, attention_mask, past_key_values, inputs_embeds, labels = \
+                self.prepare_inputs_labels_for_multimodal(input_ids, attention_mask, past_key_values, labels, images)
 
         # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
         outputs = self.model(

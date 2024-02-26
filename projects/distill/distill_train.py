0a155d1cbad2b6ef27a7fdf35d2c6efe86d3eecf
Haobo Yuan <yuanhaobo@whu.edu.cn>
2024-02-26 11:04:32 +0800

    add pretrain config

--- projects/distill/distill_train.py
+++ projects/distill/distill_train.py
@@ -222,7 +222,7 @@ class LazySupervisedDataset(Dataset):
             data_dict['stu_image'] = torch.zeros(3, stu_crop_size['height'], stu_crop_size['width'])
             data_dict['tea_image'] = torch.zeros(3, tea_crop_size['height'], tea_crop_size['width'])
         return data_dict
-    
+
 
 @dataclass
 class DataCollatorForSupervisedDataset(object):
@@ -277,7 +277,7 @@ def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer,
 
 def unlock_vit(training_args, model_args, vision_tower):
     # lr_of_vit = training_args.vision_tower_lr if training_args.vision_tower_lr is not None and training_args.vision_tower_lr != 0 else training_args.learning_rate
-    
+
     # rank0_print(f'Tune the vision tower! LR for ViT is {lr_of_vit}.')
     if model_args.tune_vit_from_layer != -1:
         rank0_print(f'Tune the vision tower from layer {model_args.tune_vit_from_layer}!')
@@ -418,7 +418,7 @@ def train():
             model_args=model_args,
             fsdp=training_args.fsdp
         )
-        
+
         vision_tower = model.get_vision_tower()
         vision_tower.to(dtype=compute_dtype, device=training_args.device)
 
@@ -483,7 +483,7 @@ def train():
                         module = module.to(torch.bfloat16)
 
     teacher_model = LlavaLlamaForCausalLM.from_pretrained(
-        "checkpoints/llava-v1.5-13b",
+        "liuhaotian/llava-v1.5-13b",
     )
 
     teacher_model.config.use_cache = False
@@ -496,7 +496,7 @@ def train():
     teacher_model.config.mm_use_im_patch_token = model_args.mm_use_im_patch_token
 
     teacher_tokenizer = transformers.AutoTokenizer.from_pretrained(
-        "checkpoints/llava-v1.5-13b",
+        "liuhaotian/llava-v1.5-13b",
         model_max_length=training_args.model_max_length,
         use_fast=False,
         padding_side="right"
@@ -510,7 +510,7 @@ def train():
 
     data_module = make_supervised_data_module(tokenizer=tokenizer,
                                               data_args=data_args)
-    
+
 
     # from llava.train.llava_trainer import LengthGroupedSampler
     # from torch.utils.data import DataLoader
@@ -654,6 +654,6 @@ class Colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-    
+
 if __name__ == "__main__":
     train()

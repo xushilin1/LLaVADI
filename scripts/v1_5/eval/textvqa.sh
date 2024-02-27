48900e538d80b6d50f982cdb0f158faf693500f6
Haobo Yuan <yuanhaobo@whu.edu.cn>
2024-02-27 15:21:14 +0800

    add textvqa

--- scripts/v1_5/eval/textvqa.sh
+++ scripts/v1_5/eval/textvqa.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 MODEL_PATH=$1
 
-PYTHONPATH='.' srun -p s1_mm_research --quotatype=auto --gres=gpu:8 --ntasks-per-node=1 --nodes=1 \
+CUDA_HOME=$CONDA_PREFIX PYTHONPATH=. srun -p ${PARTITION} --quotatype=spot --job-name=${JOB_NAME} --gres=gpu:1 --ntasks-per-node=1 --nodes=1 \
 deepspeed llava/eval/model_vqa_loader.py \
     --model-path ${MODEL_PATH} \
-    --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
-    --image-folder ./datasets/textvqa/train_images \
-    --answers-file ./playground/data/eval/textvqa/answers/llava-v1.5-13b.jsonl \
+    --question-file ./data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
+    --image-folder ./data/LLaVA-Instruct/textvqa/train_images \
+    --answers-file ./data/eval/textvqa/answers/dummy.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 python -m llava.eval.eval_textvqa \
-    --annotation-file ./playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
-    --result-file ./playground/data/eval/textvqa/answers/llava-v1.5-13b.jsonl
+    --annotation-file ./data/eval/textvqa/TextVQA_0.5.1_val.json \
+    --result-file ./data/eval/textvqa/answers/dummy.jsonl
 
-rm -rf ./playground/data/eval/textvqa/answers
+rm -rf ./data/eval/textvqa/answers

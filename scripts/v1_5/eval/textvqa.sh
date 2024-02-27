6e9112992c1e28819ad0b66aae71c382d8044992
Haobo Yuan <yuanhaobo@whu.edu.cn>
2024-02-27 16:43:41 +0800

    add textvqa

--- scripts/v1_5/eval/textvqa.sh
+++ scripts/v1_5/eval/textvqa.sh
@@ -2,7 +2,7 @@
 MODEL_PATH=$1
 
 CUDA_HOME=$CONDA_PREFIX PYTHONPATH=. srun -p ${PARTITION} --quotatype=spot --job-name=${JOB_NAME} --gres=gpu:8 --ntasks-per-node=8 --nodes=1 \
-python llava/eval/model_vqa_loader_old.py \
+python llava/eval/model_vqa_loader.py \
     --model-path ${MODEL_PATH} \
     --question-file ./data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
     --image-folder ./data/LLaVA-Instruct/textvqa/train_images \
